a83fc4bf34b272981eb7b4467de4634f5739ae60
Conrado Jardim <conradocjo@gmail.com>
2023-02-04 11:35:57 -0300

    Fix: Correção de bugs

--- src/main/java/embosse/format_name/EmbosseNameFormater.java
+++ src/main/java/embosse/format_name/EmbosseNameFormater.java
@@ -2,6 +2,10 @@ package embosse.format_name;
 
 import java.util.List;
 
+/**
+ * @author Conrado Jardim de Oliveira
+ * @version 0.0.1
+ * */
 public interface EmbosseNameFormater {
 
     public String getFormatedName(List<String> splitedName);
