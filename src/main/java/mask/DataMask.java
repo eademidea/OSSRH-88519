a04edf5cbf2169fa4c2c34efa90fd460abde389f
ericafran <erica.francielle.27@gmail.com>
2023-03-08 20:30:19 -0300

    ajustes

--- src/main/java/mask/DataMask.java
+++ src/main/java/mask/DataMask.java
@@ -1,12 +1,21 @@
 package mask;
 
 import java.lang.String;
-
+import static org.apache.commons.lang3.ObjectUtils.isEmpty;
+/**
+ * @author erica
+ */
 public class DataMask {
-
+    /**
+     * Devolve uma string mascarada, sendo exibidos apenas os dois primeiros e os dois últimos caracteres
+     * de maneira íntegra o restante da string é ocultada por um X em seus caracteres.
+     * Exemplo:
+     * String recebida: Cristina Caldeia
+     * String devolvida: CrXXXXXXXXXXXXia
+     */
     public String dataMasking(String data) {
 
-
+        handleWhenStringIsNull(data);
       String firstCharacters=data.substring(0,2);
       String lastCharacters =data.substring(data.length ()-2);
 
@@ -18,6 +27,12 @@ public class DataMask {
         return newData;
     }
 
+    /**
+     * Devolve uma string com todos os seus caracteres substituídos por X.
+     * Exemplo:
+     * STring recebida: Envelope
+     * String devolvida: XXXXXXXX
+     */
     public String insertX(String data) {
         String dataMask = "";
         for (int i = 0; i <data.length(); i++) {
@@ -26,6 +41,12 @@ public class DataMask {
         }
 
         return dataMask;
+    }
+
+    private static void handleWhenStringIsNull(String data){
+        if(isEmpty(data)){
+            throw new RuntimeException("Dado passado é vazio ou nulo");
+        }
     }
 
 
